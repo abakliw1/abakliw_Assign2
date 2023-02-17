c15e4bec34eef83ae020c05c9f27ae5c033f226b
Akshat Bakliwal <abakliw1@asu.edu>
2023-02-16 18:43:21 -0700

    Added the + operation with passed test case

--- src/main/java/MyCipher.java
+++ src/main/java/MyCipher.java
@@ -2,7 +2,7 @@ public class MyCipher {
     public int compute(String str){
         String[] val= str.split(" ");
         switch (val[0]){
-            case "+": return Integer.valueOf(val[1])/Integer.valueOf(val[2]);
+            case "+": return Integer.valueOf(val[1])+Integer.valueOf(val[2]);
         }
         return 0;
     }
