6fd2e2ac9d4691b1ae02fa5336942e495270f97f
Akshat Bakliwal <abakliw1@asu.edu>
2023-02-16 18:51:48 -0700

    Added the * operation with passed test case

--- src/main/java/MyCipher.java
+++ src/main/java/MyCipher.java
@@ -4,7 +4,7 @@ public class MyCipher {
         switch (val[0]){
             case "+": return Integer.valueOf(val[1])+Integer.valueOf(val[2]);
             case "-": return Integer.valueOf(val[1])-Integer.valueOf(val[2]);
-            case "*": return Integer.valueOf(val[2])*Integer.valueOf(val[2]);
+            case "*": return Integer.valueOf(val[1])*Integer.valueOf(val[2]);
         }
         return 0;
     }
